0e722b4d0bc9f6d3203d09cba284c2ce3bb0f6a9
uvcodin <129361543+uvcodin@users.noreply.github.com>
2023-04-02 12:48:27 +0530

    Update python_assignment_4.py

--- python_assignment_4.py
+++ python_assignment_4.py
@@ -18,3 +18,20 @@ greet_user = lambda name : print('Hey there,', name)
 
 greet_user('Yuvi')          #prints "Yuvi"
 
+#Reading Mode
+
+f = open("test_file.txt","r")
+data=f.read()
+print(data)
+hello there
+
+#Writting Mode
+
+file = open("test_file.txt","w")
+data=file.write("hello again")
+file.close()
+
+#Append Mode
+# Open the file in append mode
+with open('test_file.txt', 'a') as file:
+    file.write('\nbye')
